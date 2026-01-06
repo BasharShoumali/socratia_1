2ba511a1b6be8c8c8cd6ec114a7f71e92875f62f
Bashar Shoumali <basharshomaly1@hotmail.com>
2026-01-06 15:34:08 +0200

    unused old files deleted

--- server/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { chatWithAssistant } from "../controllers/chat.controller.js";
-
-const router = express.Router();
-
-router.post("/", chatWithAssistant);
-
-export default router;
